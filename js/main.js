0f23500b57a7db082f5eea56d9895773772bd9d6
William Dias <smdwilliam89@gmail.com>
2023-04-02 20:25:08 +0100

    Consertando o CSS e o JS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-const timeTrackDaily = fetch('../data.json')
+const data = fetch('../data.json')
   .then((res) => res.json())
   .then((data) => {
-    cardDetail.html(
-      data.map((time) => {
-        return `
-          <div class="card-detail card-work">
-            <h2 class="card-title title-work">${time.title}</h2>
-            <span class="ellipsis-icon"><img src="./images/icon-ellipsis.svg" alt=""></span>
-            <p class="currentHours">${time.timeframes.daily.current}hrs</p>
-            <p class="previous-hours">Last Week - ${time.timeframes.daily.previous}</p>
-          </div>
+    const cards = data.map((item) => {
+      const divCard = document.createElement('div');
+      divCard.classList.add('card-detail');
+
+      divCard.innerHTML = `
+        <h2 class="card-title">${item.title}</h2>
+        <span class="ellipsis-icon"><img src="./images/icon-ellipsis.svg" alt=""></span>
+        <p class="current-hours">${item.timeframes.daily.current}hrs</p>
+        <p class="previous-hours">Last Week - ${item.timeframes.daily.previous}hrs</p>
       `;
-      }),
-    );
+      return divCard;
+    });
+
+    const individualCard = document.querySelector('.card');
+    cards.forEach((divCard) => individualCard.appendChild(divCard));
+
+    const daily = document.querySelector('.daily');
+    daily.addEventListener('click', () => {});
   });
-
-const cardDetail = $('.individual-card');
-const daily = $('.daily');
-const weekly = $('.weekly');
-const monthly = $('.monthly');
-
-$(document).ready(() => {});
